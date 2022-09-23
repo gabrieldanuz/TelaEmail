0f9b38c2ae0ba453bff8660db1ca58494326a40e
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-09-22 21:45:36 -0300

    Iniciando

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+
+  function sendEmail(e){
+    e.preventDefault();
+
+    if(name === '' || email === '' || message === ''){
+      
+      alert("Preencha todos os campos");
+      return;
+
+    }
+
+    alert('TESTE')
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Contato</h1>
+
+      <form className="form" onSubmit={sendEmail}>
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        
+        <input 
+          className="input"
+          type="text"
+          placeholder="Digite seu email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <textarea 
+          className="textarea"
+          placeholder="Digite sua mensagem..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <input className="button" type="submit" value="Enviar" />
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
